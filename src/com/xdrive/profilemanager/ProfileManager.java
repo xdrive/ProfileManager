b59befc7d40d56941452f481d3d2b1625b9e87ef
Dmytro Kovalenko <dmytro.kovalenko@gmail.com>
2010-11-03 10:23:27 +0200

    initial commit

---
+++ src/com/xdrive/profilemanager/ProfileManager.java
@@ -0,0 +1,13 @@
+package com.xdrive.profilemanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfileManager extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
