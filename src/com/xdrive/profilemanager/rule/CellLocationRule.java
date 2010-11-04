2b5e04595747dd961e8e4d0885ac401f1f054ff0
Dmytro Kovalenko <dmytro.kovalenko@gmail.com>
2010-11-04 23:32:51 +0200

    starting with rules implementation

---
+++ src/com/xdrive/profilemanager/rule/CellLocationRule.java
@@ -0,0 +1,11 @@
+package com.xdrive.profilemanager.rule;
+
+import com.xdrive.profilemanager.location.Cell;
+
+public class CellLocationRule implements LocationRule {
+	private Cell[] cells;
+
+	public void addCell(Cell cell) {
+		// @TODO: add implementation
+	}
+}
