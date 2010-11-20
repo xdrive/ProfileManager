804907fcee76bac163c007e0955bbff71f735cf3
Dmytro Kovalenko <dmytro.kovalenko@gmail.com>
2010-11-20 22:28:49 +0200

    adding fetchActiveConditions method to ConditionManager that fetches all conditions and their rules

--- src/com/xdrive/profilemanager/condition/ConditionManager.java
+++ src/com/xdrive/profilemanager/condition/ConditionManager.java
@@ -1,19 +1,30 @@
 package com.xdrive.profilemanager.condition;
 
+import java.sql.SQLException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-import android.R.bool;
+import com.xdrive.profilemanager.ProfileManager;
+import com.xdrive.profilemanager.data.DatabaseHelper;
+import com.xdrive.profilemanager.rule.Rule;
+
+import android.util.Log;
 
 /**
- * Manages conditions. checkes them and if they meet their rules activates 
+ * Manages conditions. Checks them and if they meet their rules activates 
  * profiles
  * @author Dmytro_Kovalenko <dmytro_kovalenko@gmail.com>
  *
  */
 public class ConditionManager {
 	private Set<Condition> conditions = new HashSet<Condition>();
+	private DatabaseHelper helper;
 
+	public ConditionManager(DatabaseHelper helper) {
+		this.helper = helper;
+	}
+	
 	public Set<Condition> getConditions() {
 		return conditions;
 	}
@@ -21,6 +32,21 @@ public class ConditionManager {
 	public void setConditions(Set<Condition> conditions) {
 		this.conditions = conditions;
 	}	
+	
+	public void fetchActiveConditions() {
+		try {
+			List<Condition> conditionsList = helper.getConditionDAO().queryForAll();
+			conditions = new HashSet<Condition>(conditionsList);
+			for(Condition condition : conditions) {
+				Set<Rule> rules = helper.getConditionRules(condition);
+				condition.setRules(rules);
+			}
+		} catch (SQLException e) {
+			Log.e(ProfileManager.class.getSimpleName(), "Error fetching conditions");
+		}
+		
+		
+	}
 	
 	public void checkRules() {
 		for(Condition c : conditions) {
